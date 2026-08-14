652be1dc12463e6a9c03f7f29ba12f2d7aeab67d
Sriramdev1604 <sriramvenkatesandev@gmail.com>
2026-08-14 10:53:35 +0530

    Primary task done

--- index.js
+++ index.js
@@ -8,4 +8,10 @@ function helloFunction(name,a,b){
 
     console.log(add(a,b));
 
+    //primary task subraction method also been added successfully.
+    const sub = (a,b) => a - b;
+
+    console.log(sub(a,b));
+
 } 
+
